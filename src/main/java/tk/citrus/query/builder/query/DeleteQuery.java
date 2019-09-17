b542b21097876698756a4874b25d629a4897f25e
kouhei.takemoto <take64@citrus.tk>
2019-09-18 00:15:32 +0900

    DELETEクエリー

---
+++ src/main/java/tk/citrus/query/builder/query/DeleteQuery.java
@@ -0,0 +1,134 @@
+package tk.citrus.query.builder.query;
+
+/*
+ * @copyright   Copyright 2019, Citrus/besidesplus All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+import tk.citrus.query.builder.QueryTypes;
+import tk.citrus.query.builder.parse.Order;
+import tk.citrus.query.builder.parse.Where;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * INSERT
+ */
+public class DeleteQuery extends QueryStruct<DeleteQuery>
+{
+
+    /**
+     * WHERE句
+     */
+    private List<Where> wheres = new ArrayList<>();
+
+    /**
+     * ORDER BY句
+     */
+    private List<Order> orders = new ArrayList<>();
+
+
+
+    /**
+     * constructor.
+     */
+    public DeleteQuery()
+    {
+        this.setQueryType(QueryTypes.DELETE);
+    }
+
+
+
+    /**
+     * テーブル設定
+     *
+     * @param table テーブル名
+     * @return メソッドチェイン用の自身返却
+     */
+    public DeleteQuery from(String table)
+    {
+        this.setTable(table);
+
+        return this;
+    }
+
+
+
+    /**
+     * 比較子指定のWHERE句
+     *
+     * @param column   カラム名
+     * @param operator 比較
+     * @param value    値
+     * @return メソッドチェイン用の自身返却
+     */
+    public DeleteQuery where(String column, String operator, Object value)
+    {
+        this.getWheres().add(Where.generate(column, operator, value));
+
+        return this;
+    }
+
+
+
+    /**
+     * 単純なイコールのWHERE句
+     *
+     * @param column カラム名
+     * @param value  値
+     * @return メソッドチェイン用の自身返却
+     */
+    public DeleteQuery where(String column, Object value)
+    {
+        return this.where(column, Where.OPERATOR_EQUAL, value);
+    }
+
+
+
+    @Override
+    public String callQuery()
+    {
+        // 文字列作成
+        StringBuffer buffer = new StringBuffer("");
+
+        // FROM句まで
+        buffer.append(this.getQueryType())
+                .append(" FROM ")
+                .append(this.getTable());
+
+        // WHERE句
+        buffer.append(Where.query(this.getWheres()));
+
+        return buffer.append(";").toString();
+    }
+
+
+
+    public List<Where> getWheres()
+    {
+        return wheres;
+    }
+
+
+
+    public void setWheres(List<Where> wheres)
+    {
+        this.wheres = wheres;
+    }
+
+
+
+    public List<Order> getOrders()
+    {
+        return orders;
+    }
+
+
+
+    public void setOrders(List<Order> orders)
+    {
+        this.orders = orders;
+    }
+}
---
+++ src/test/java/tk/citrus/query/builder/query/DeleteQueryTest.java
@@ -0,0 +1,30 @@
+package tk.citrus.query.builder.query;
+
+/*
+ * @copyright   Copyright 2019, Citrus/besidesplus All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteQueryTest
+{
+    @Test
+    void 生成_SQLが生成できる()
+    {
+        DeleteQuery deleteQuery = new DeleteQuery();
+
+        // クエリを生成して取得
+        String query = deleteQuery.table("employees")
+                .where("employee_id", 1000)
+                .where("name", "jhon")
+                .callQuery();
+
+        // 想定通りか
+        assertEquals("DELETE FROM employees WHERE employee_id = ? AND name = ?;", query);
+    }
+
+}
